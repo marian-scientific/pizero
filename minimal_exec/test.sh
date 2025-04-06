bda71a97a798b23d97c830619bafaa932967f47a
xmdi <xmdi@protonmail.com>
2025-04-05 22:26:10 -0400

    minimal arm works!

---
+++ minimal_exec/test.sh
@@ -0,0 +1,12 @@
+as code.asm -o temp.o
+ld temp.o -o temp -w
+
+bytes=$(xxd -p -l8 -g4 -s 216 -e temp)
+bytes_trimmed="${bytes:19:8}${bytes:10:8}"
+bytes_to_end=$((16#${bytes_trimmed}+120))
+
+xxd -p -s 120 -l "$bytes_to_end" temp | xxd -r -p > binary
+
+rm temp*
+chmod +x binary
+sudo ./binary
